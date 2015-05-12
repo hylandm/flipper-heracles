5d10e62d8f69bde41e97ec5bbe407a4013aef198
Isaac Shivvers <ishivvers@berkeley.edu>
2015-05-12 16:51:46 -0700

    a script to add snid matches and snr to every spectrum. run in this way May 12

--- snid_sn_everything.py
+++ snid_sn_everything.py
@@ -6,6 +6,8 @@ from scriptsLib import yield_all_spectra, get_info_spec_fitsfile
 import MySQLdb
 import MySQLdb.cursors
 from os import path
+import credentials as creds
+import numpy as np
 
 DB = MySQLdb.connect(host=creds.host, user=creds.user, passwd=creds.passwd, db='newsndb', cursorclass=MySQLdb.cursors.DictCursor)
 MAX_RESULTS = 100
@@ -20,7 +22,7 @@ def getSNID( flmfile ):
     o,e = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE).communicate()
     # if SNID found no matches at all, quit here
     if re.search('Thank you for using SNID! Goodbye.',o) == None:
-        return None
+        return 'NoMatch','NoMatch'
     typestring,subtypestring = o.split('Best subtype(s)')
     # find the best types first
     lines = typestring.split('\n')
@@ -46,16 +48,40 @@ def getSN( flmfile, w=20 ):
     """
     Calculates the S/N ratio at the central wavelength, in a 
      bin +/-<w> angstroms wide.
+    Will remove a polynomial fit before calculating noise.
     """
     d = np.loadtxt( flmfile )
     midwl = d[len(d)/2, 0]
     m = (d[:,0] > midwl-w) & (d[:,0] < midwl+w)
-    return np.mean( d[:,1][m] )/np.std( d[:,1][m] )
+    # fit and subtract a line from noise
+    p = np.poly1d( np.polyfit( d[:,0][m], d[:,1][m], 3 ) )
+    std = np.std( d[:,1][m] - p(d[:,0][m]) )
+    return np.mean( d[:,1][m] ) / std
 
 def getBoth( flmfile ):
-    snr = getSN( flmfile )
-    t,st = getSNID( flmfile )
+    try:
+        snr = getSN( flmfile )
+    except:
+        snr = 0.0
+    try:
+        t,st = getSNID( flmfile )
+    except:
+        t,st = 'NoMatch','NoMatch'
     return t,st,snr
+
+def insertIntoDB( t,st,snr,specID ):
+    sql = 'UPDATE spectra SET SNID_Type="%s",SNID_Subtype="%s",SNR=%f WHERE SpecID=%d;' %(t,st,snr,specID)
+    c = DB.cursor()
+    print sql
+    c.execute( sql )
+    c.close()
+    DB.commit()
+
+def do_it_all( inn ):
+    flmfile, specid = inn
+    t,st,snr = getBoth( flmfile )
+    insertIntoDB( t,st,snr,specid )
+    
 
 def runAllFlipperSpec( skipstrs=[], rootdir='/media/raid0/Data/spectra/', maxnum=None ):
     """
@@ -68,16 +94,17 @@ def runAllFlipperSpec( skipstrs=[], rootdir='/media/raid0/Data/spectra/', maxnum
     fs = []
     for i,s in enumerate(S):
         # make sure that this spectrum is in the DB
-        fname = path.basedir( s[0] )
-        c.execute( 'SELECT SpecID,Filename FROM spectra WHERE Filename = "%s";'%fname )
-        SpecID = c.fetchone().get('SpecID')
-        if SpecID == None:
-            open('snid_missing_DB.txt','a').write('%s\n'%s[0])
+        fname = path.basename( s[0] )
+        c.execute( 'SELECT SpecID,Filename FROM spectra WHERE (Filename = "%s" AND SNID_Type IS NULL);'%fname )
+        res = c.fetchone()
+        if res == None:
+            #open('snid_missing_DB.txt','a').write('%s\n'%s[0])
             continue
+        SpecID = res.get('SpecID')
         # don't include any files that match one of the skipstrs
         if not any( [1 if ss in fname else 0 for ss in skipstrs] ):
             fs.append( [s[0], SpecID] )
-        if (maxnum!=None) and (i>=maxnum):
+        if (maxnum!=None) and (len(fs)>=maxnum):
             break
     c.close()
     # now run all files in parallel
@@ -86,15 +113,8 @@ def runAllFlipperSpec( skipstrs=[], rootdir='/media/raid0/Data/spectra/', maxnum
     # start the processor pool
     print 'starting SNID calculations...'
     pool = multiprocessing.Pool( multiprocessing.cpu_count() )
-    results = pool.map( runBoth, [f[0] for f in fs] )
-    
-    # insert the results into the DB
-    c = DB.cursor()
-    for i,res in enumerate(results):
-        s = fs[i]
-        sql = 'UPDATE spectra SET SNID_Type="%s",SNID_Subtype="%s",SNR=%f WHERE SpecID %d;' %(res[0], res[1], res[2], s[1])
-        # c.execute( sql )
-        # DB.commit()
-    c.close()
-    
+    pool.map( do_it_all, fs )
+    #map( do_it_all, fs )
+
+   
 
