92eb44b17c4d0ddb7ac58727ff67964f2a5c26d7
Isaac Shivvers <ishivvers@berkeley.edu>
2015-05-18 16:42:59 -0700

    re-ran photometry insertion with this code; seemed to be alright

--- insert_all_phot.py
+++ insert_all_phot.py
@@ -20,7 +20,7 @@ NPoints INT(4),
 Notes TEXT,
 Reference TEXT,
 DateReduced DATE,
-Public TINYINT NOT NULL DEFAULT 0
+Public TINYINT(1) NOT NULL DEFAULT 0
 );
 """
 from glob import glob
@@ -33,6 +33,8 @@ import julian_dates as jd
 
 DB = MySQLdb.connect(host=creds.host, user=creds.user, passwd=creds.passwd, db='newsndb', cursorclass=MySQLdb.cursors.DictCursor)
 c = DB.cursor()
+oDB = MySQLdb.connect(host='classy.astro.berkeley.edu', user=creds.user, passwd=creds.passwd, db='sndb', cursorclass=MySQLdb.cursors.DictCursor)
+oc = oDB.cursor()
 
 def parse_photfile( f ):
     lines = [l for l in open(f,'r').readlines() if l[0]!='#']
@@ -52,13 +54,18 @@ def insert_photfile( f ):
     fpath,fname = os.path.split( f )
     # trim the fpath relative to the Data directory
     fpath = fpath.split('raid0/')[1]
-    # find the object
-    objname = fname.split('.')[0].replace('sn','SN ')
+    # find the object row
+    objname = fname.split('.')[0]
+    if objname[:2] == 'sn':
+        objname = objname[2:]
+    # have it match against lower or upper case if it's a single letter
+    if len(objname) == 5:
+        objname = objname[:-1] + '[%s%s]$'%(objname[-1].lower(),objname[-1].upper())
     # see if there's a related file in the old DB
-    cmd = "SELECT t1.* FROM old_photometry as t1, objects as t2 WHERE ((t1.ObjID = t2.ObjID) AND (t2.ObjName = '%s'));" %objname
+    cmd = "SELECT t1.* FROM photometry as t1, objects as t2 WHERE ((t1.ObjID = t2.ObjID) AND (t2.ObjName REGEXP '%s'));" %objname
     print cmd
-    c.execute(cmd)
-    res = c.fetchone()
+    oc.execute(cmd)
+    res = oc.fetchone()
     if res != None:
         # this object is in old database; pull reference info if it exists
         ref = res['Reference']
@@ -66,8 +73,21 @@ def insert_photfile( f ):
             ref = 'NULL'
     else:
         ref = 'NULL'
-    # get object ID if you can
-    cmd = "SELECT * from objects WHERE (ObjName = '%s');"%objname
+    if ref == 'NULL':
+        # try everything again from the public objects table
+        cmd = "SELECT t1.* FROM photometry_public as t1, objects_public as t2 WHERE ((t1.ObjID = t2.ObjID) AND (t2.ObjName REGEXP '%s'));" %objname
+        print cmd
+        oc.execute(cmd)
+        res = oc.fetchone()
+        if res != None:
+            # this object is in old database; pull reference info if it exists
+            ref = res['Reference']
+            if ref == None:
+                ref = 'NULL'
+        else:
+            ref = 'NULL'
+
+    cmd = "SELECT * from objects WHERE (ObjName REGEXP '%s');"%objname
     c.execute(cmd)
     res = c.fetchone()
     if res != None:
